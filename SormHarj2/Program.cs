6dfdcfb4bbb0264f9acfa3dcce6e76a38e75f6fd
Teemu Mäkinen <K2001@Labranet.jamk.fi>
2017-08-30 10:43:51 +0300

    Add project files.

---
+++ SormHarj2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SormHarj2
+{
+    class Program
+    {
+        static void DrawLotto()     //Arvotaan 7 numeroa väliltä 1-40
+        {
+            Console.WriteLine("Montako riviä pelataan (Väh. 3)? ");
+            int NumberOfLines = int.Parse(Console.ReadLine());
+
+            if (NumberOfLines < 3)
+            {
+                Console.WriteLine("Rivien vähimmäismäärä on 3.");
+                Console.WriteLine("Montako riviä pelataan (Väh. 3)?");
+                NumberOfLines = int.Parse(Console.ReadLine());
+            }
+
+            int[] Numbers = new int[NumberOfLines];
+
+        }
+        static void Main(string[] args)
+        {
+            DrawLotto();
+        }
+    }
+}
