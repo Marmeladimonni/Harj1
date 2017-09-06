6809032ac17bc6a551c0829f9f6be0e9f2e0d0bc
Teemu Mäkinen <K2001@student.jamk.fi>
2017-09-06 08:16:50 +0300

    asdf

--- SormHarj2/Program.cs
+++ SormHarj2/Program.cs
@@ -10,11 +10,14 @@ namespace SormHarj2
     {
         static void DrawLotto()     //Arvotaan 7 numeroa väliltä 1-40
         {
+            int NumberOfLines;
+            
             Console.WriteLine("Montako riviä pelataan (Väh. 3)? ");
-            int NumberOfLines = int.Parse(Console.ReadLine());
+            NumberOfLines = int.Parse(Console.ReadLine());
 
             if (NumberOfLines < 3)
             {
+                NumberOfLines = 0;
                 Console.WriteLine("Rivien vähimmäismäärä on 3.");
                 Console.WriteLine("Montako riviä pelataan (Väh. 3)?");
                 NumberOfLines = int.Parse(Console.ReadLine());
